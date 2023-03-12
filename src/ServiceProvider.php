f69a7fd7107413aea98d26879720896128c01056
Brian Faust <hello@basecode.sh>
2023-03-12 06:00:50 +0200

    ci: update configurations

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,17 +4,15 @@ declare(strict_types=1);
 
 namespace PreemStudio\Identicon;
 
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use PreemStudio\Jetpack\Package\AbstractServiceProvider;
+use PreemStudio\Jetpack\Package\Package;
 
-class ServiceProvider extends PackageServiceProvider
+class ServiceProvider extends AbstractServiceProvider
 {
     public function configurePackage(Package $package): void
     {
         $package
             ->name('laravel-identicon')
-            ->hasConfigFile('laravel-identicon')
-            ->hasInstallCommand(fn (InstallCommand $command) => $command->publishConfigFile());
+            ->hasConfigFile('laravel-identicon');
     }
 }
